0263af55a51d7dabb86febf6509d6f4b76112299
joaoleandro <new.joaoleandro@gmail.com>
2023-10-05 16:01:21 -0300

    formatando valores 2

--- extras/format.py
+++ extras/format.py
@@ -1,6 +1,18 @@
+#  :s - texto string
+#  :d - números inteiros
+#  :f - números flutuantes
+#  :.(NUMERO)f - quantidade de casas decimais Float
+#  :(CARACTERE) (> ou < ou ^) (QUANTIDADE) (TIPO - s d ou f)
+
 numero_1 = 10
 numero_2 = 3
 
 divisao = numero_1 / numero_2
 print(divisao)
-print('{:.3f}'.format(divisao))
+print('{:.2f}'.format(divisao))
+
+teste = "numero Final"
+print(f'{teste} {divisao:.1f}')
+
+nome = "João Leandro"
+print(f'{nome:.3s}')
