e51f374225b972fb9da0bbaac25a0ac0912e7294
joaoleandro <new.joaoleandro@gmail.com>
2023-10-17 21:04:37 -0300

    calc

---
+++ condicoes/calculadora.py
@@ -0,0 +1,13 @@
+operation = "+"
+valor1 = 20
+valor2 = 20
+if operation == "+":
+    print(valor1 + valor2)
+elif operation == "-":
+    print(valor1 - valor2)
+elif operation == "*":
+    print(valor1 * valor2)
+elif operation == "/":
+    print(valor1 / valor2)
+else:
+    print("Operação não encontrada.")
