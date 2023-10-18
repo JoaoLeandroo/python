cd8d1696ed44013e0a8cf1e503fe0dd35fb37663
joaoleandro <new.joaoleandro@gmail.com>
2023-10-17 21:09:42 -0300

    elif

---
+++ condicoes/elif.py
@@ -0,0 +1,12 @@
+pais = "Estados unidos"
+
+if pais.upper() == "BRASIL":
+    print("Voce é Brasileiro")
+elif pais.upper() == "ARGENTINA":
+    print("Voce é Argentino")
+elif pais.upper() == "MEXICO":
+    print("Voce é Mexicano")
+elif pais.upper() == "ESTADOS UNIDOS":
+    print("Voce é Americano")
+else:
+    print("Seu pais ainda não foi adicionado.")
