d84ec1e83bcb69cc6395ff7e889d2f07a06a26ff
joaoleandro <new.joaoleandro@gmail.com>
2023-11-16 10:28:02 -0300

    exercicio 05

--- Atividades/Exercicio05.py
+++ Atividades/Exercicio05.py
@@ -2,34 +2,36 @@ import matplotlib.pyplot as plt
 
 # Dados fornecidos
 sst = [
-    [1995, 0.47], [1996, 0.32], [1997, 0.52], [1998, 0.65], [1999, 0.44],
-    [2000, 0.43], [2001, 0.57], [2002, 0.62], [2003, 0.64], [2004, 0.58],
-    [2005, 0.67], [2006, 0.64], [2007, 0.62], [2008, 0.54], [2009, 0.65],
-    [2010, 0.73], [2011, 0.58], [2012, 0.64], [2013, 0.68], [2014, 0.74],
-    [2015, 0.93], [2016, 1], [2017, 0.91], [2018, 0.83]
+    [1995, -1.06216], [1996, -1.99387], [1997, -0.75158], [1998, 0.0559], [1999, -1.2485], [2000, -1.31061], 
+    [2001, -0.44101], [2002, -0.13044], [2003, -0.00621], [2004, -0.3789], [2005, 0.18013], [2006, -0.00621], 
+    [2007, -0.13044], [2008, -0.62736], [2009, 0.0559], [2010, 0.55282], [2011, -0.3789], [2012, -0.00621], 
+    [2013, 0.24225], [2014, 0.61493], [2015, 1.79511], [2016, 2.22991], [2017, 1.67088], [2018, 1.17396]
 ]
 
 mediaTotal_chmsl = [
-    [1995, -21.736], [1996, -19.472], [1997, -16.32], [1998, -17.973], [1999, -14.86], [2000, -11.849],
-    [2001, -6.201], [2002, -2.907], [2003, -0.151], [2004, 2.767], [2005, 7.774], [2006, 8.934], [2007, 8.921],
-    [2008, 12.586], [2009, 17.027], [2010, 18.391], [2011, 17.632], [2012, 28.173], [2013, 30.603], [2014, 34.586],
-    [2015, 44.455], [2016, 47.175], [2017, 48.573], [2018, 52.683]
+    [1995, -1.41698], [1996, -1.31958], [1997, -1.18397], [1998, -1.25509], [1999, -1.12116], [2000, -0.99162],
+    [2001, -0.74863], [2002, -0.60692], [2003, -0.48835], [2004, -0.36281], [2005, -0.14739], [2006, -0.09749], 
+    [2007, -0.09805], [2008, 0.05963], [2009, 0.25069], [2010, 0.30937], [2011, 0.27672], [2012, 0.73022], 
+    [2013, 0.83476], [2014, 1.00612], [2015, 1.43071], [2016, 1.54773], [2017, 1.60787], [2018, 1.78469]
 ]
 
-# Extrai anos e valores das listas
+# Os anos e valores são extraídos das listas usando a função zip e a descompactação do operador *. 
+# Isso cria quatro listas: anos_sst, valores_sst, anos_media, e valores_media.
 anos_sst, valores_sst = zip(*sst)
 anos_media, valores_media = zip(*mediaTotal_chmsl)
 
-# Plota os dados
+# Duas linhas são plotadas no gráfico usando os dados extraídos. A opção 'o-' e 's-' especifica o estilo da linha, 
+# onde 'o' e 's' representam marcadores circulares e quadrados, respectivamente.
 plt.plot(anos_sst, valores_sst, 'o-', label='SST')  # 'o-' adiciona marcadores circulares nos pontos
 plt.plot(anos_media, valores_media, 's-', label='Média Total CHMSL')  # 's-' adiciona marcadores quadrados nos pontos
 
-# Adiciona rótulos aos eixos e título
+# Rótulos são adicionados aos eixos x e y, e um título é atribuído ao gráfico.
 plt.xlabel('Ano')
 plt.ylabel('Valor Padronizado')
 plt.title('Plot das Variáveis Padronizadas')
 
-# Adiciona rótulos aos pontos no gráfico
+# Rótulos são adicionados a cada ponto no gráfico, exibindo os valores correspondentes. 
+# Os rótulos são posicionados acima dos pontos para a variável SST e abaixo para a Média Total CHMSL.
 for i, txt in enumerate(valores_sst):
     plt.annotate(f'{txt:.2f}', (anos_sst[i], valores_sst[i]), textcoords="offset points", xytext=(0, 10), ha='center')
 
@@ -38,5 +40,5 @@ for i, txt in enumerate(valores_media):
 
 plt.legend()  # Adiciona a legenda com base nos rótulos usados
 
-# Exibe o gráfico
+# Exibe o gráfico na tela
 plt.show()
